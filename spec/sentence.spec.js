50f85010f8223f0bd697936385cb6e0949df5011
Katie Hoffman <ktahoffman@gmail.com>
2015-01-29 14:30:02 -0500

    remove solution

--- spec/sentence.spec.js
+++ spec/sentence.spec.js
@@ -4,11 +4,19 @@ describe('countSentences', function() {
   
   describe( "#countSentences", function() {
     it("should return the number of sentences in a string", function() {
-      expect(countSentences("one. two. three?")).toBe(3);
+      expect(countSentences("This is one. Here's two. This is three.")).toBe(3);
     });      
+
+    it("should account for question marks", function() {
+      expect(countSentences("This is one. Is this two? This is three.")).toBe(3);
+    });   
+
+    it("should account for exclamation points", function() {
+      expect(countSentences("This is one! Is this two? This is three.")).toBe(3);
+    });   
 
     it("should return zero if there are no sentences in a string", function() {
       expect(countSentences("")).toBe(0);
-    });   
+    });
   });
 });
