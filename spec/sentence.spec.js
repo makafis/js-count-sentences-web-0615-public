01a0c8d6814bd8a26ba6ac699c3d9418be3b0904
Katie Hoffman <ktahoffman@gmail.com>
2015-01-29 14:26:09 -0500

    update format of test for ironboard

--- spec/sentence.spec.js
+++ spec/sentence.spec.js
@@ -1,12 +1,7 @@
 'use strict';
 
 describe('countSentences', function() {
-
-  var countSentences;
-  beforeEach(function() {
-    countSentences = require('../lib/countSentences');
-  });
-
+  
   describe( "#countSentences", function() {
     it("should return the number of sentences in a string", function() {
       expect(countSentences("one. two. three?")).toBe(3);
